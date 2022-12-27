70f31e2e7c5601e93f1b0f93665d437780f65d0c
seriousQA <105988683+seriousQA@users.noreply.github.com>
2022-12-27 10:59:50 +0100

    Indentation style

--- SETUP/SETUPlib.cs
+++ SETUP/SETUPlib.cs
@@ -34,195 +34,196 @@ namespace SETUP
     	/// <summary>
     	/// Run 32-bit or 64-bit application.
     	/// <param name="FullExePathX86">full path to *.exe file (32-bit application).</param>
-		/// <param name="ShortBinPathX86">short path to Bin folder (32-bit application).</param>
-		/// <param name="FullExePathX64">full path to *.exe file (64-bit application).</param>
-		/// <param name="ShortBinPathX64">short path to Bin folder (64-bit application).</param>
+	/// <param name="ShortBinPathX86">short path to Bin folder (32-bit application).</param>
+	/// <param name="FullExePathX64">full path to *.exe file (64-bit application).</param>
+	/// <param name="ShortBinPathX64">short path to Bin folder (64-bit application).</param>
     	/// </summary>
     	[UserCodeMethod]
     	public static void FirstOpenApp(string FullExePathX86, string ShortBinPathX86, string FullExePathX64, string ShortBinPathX64)
     	{
-    	if (File.Exists(FullExePathX86))
-    	{
-    	    Host.Local.RunApplication(FullExePathX86, "", ShortBinPathX86, false);
-    	    Delay.Seconds(3);
-    	}
-    	else
-    	{
-    	    Host.Local.RunApplication(FullExePathX64, "", ShortBinPathX64, false);
-    	    Delay.Seconds(3);
-    	}
+    		if (File.Exists(FullExePathX86))
+    		{
+    	    		Host.Local.RunApplication(FullExePathX86, "", ShortBinPathX86, false);
+    	    		Delay.Seconds(3);
+    		}
+    		else
+    		{
+    	    		Host.Local.RunApplication(FullExePathX64, "", ShortBinPathX64, false);
+    	    		Delay.Seconds(3);
+    		}
     	}    	   		
     	
-		/// <summary>
-		/// Delete from the registry the key for application.
-		/// key name = variable
-		/// <param name="regeditFolder">the name of registry folder of your application.</param>
-		/// </summary>
+	/// <summary>
+	/// Delete from the registry the key for application.
+	/// key name = variable
+	/// <param name="regeditFolder">the name of registry folder of your application.</param>
+	/// </summary>
     	[UserCodeMethod]
     	public static void REGdelete(string regeditFolder)
     	{
-    		RegistryKey keyName  = Microsoft.Win86.Registry.CurrentUser.OpenSubKey(@"SOFTWARE", true);
+		RegistryKey keyName  = Microsoft.Win86.Registry.CurrentUser.OpenSubKey(@"SOFTWARE", true);
     		if (keyName == null)
     		{
     			
     		}
-			else 
-			{
-				keyName.DeleteSubKeyTree(regeditFolder, false);
+		else 
+		{
+			keyName.DeleteSubKeyTree(regeditFolder, false);
     			keyName.Close();
-			}
+		}
     	}
     	
-		/// <summary>
-		/// Remove such traces of a test program like \Documents, \Roaming and s.o.
-		/// <param name="regeditFolder">the name of registry folder of your application.</param>
-		/// </summary>
+	/// <summary>
+	/// Remove such traces of a test program like \Documents, \Roaming and s.o.
+	/// <param name="regeditFolder">the name of registry folder of your application.</param>
+	/// </summary>
     	[UserCodeMethod]
     	public static void RMDIR(string regeditFolder)
     	{    		
     		string pathDocuments = @"c:\%HOMEPATH%\Documents\" + regeditFolder;
     		if (Directory.Exists(Environment.ExpandEnvironmentVariables(pathDocuments)))
     		{
-    		DirectoryInfo Documents = new DirectoryInfo(Environment.ExpandEnvironmentVariables(pathDocuments));
-        	Documents.Delete(true);
+    			DirectoryInfo Documents = new DirectoryInfo(Environment.ExpandEnvironmentVariables(pathDocuments));
+        		Documents.Delete(true);
     		}
 			
-			string pathRoaming = @"c:\%HOMEPATH%\AppData\Roaming\" + regeditFolder;
-			pathRoaming = Environment.ExpandEnvironmentVariables(pathRoaming);
+		string pathRoaming = @"c:\%HOMEPATH%\AppData\Roaming\" + regeditFolder;
+		pathRoaming = Environment.ExpandEnvironmentVariables(pathRoaming);
 			
-			if (Directory.Exists(pathRoaming))
+		if (Directory.Exists(pathRoaming))
     		{
-				// search for *.ini	in folder and delete	
-				string[] fileList1 = Directory.GetFiles(pathRoaming, "*.ini");
+			// search for *.ini	in folder and delete	
+			string[] fileList1 = Directory.GetFiles(pathRoaming, "*.ini");
     			foreach (string f1 in fileList1)
-   				{
+   			{
         			File.Delete(f1);
     			}
     			// search for *.xml	in folder and delete	
-				string[] fileList2 = Directory.GetFiles(pathRoaming, "*.xml");
+			string[] fileList2 = Directory.GetFiles(pathRoaming, "*.xml");
     			foreach (string f2 in fileList2)
-   				{
+   			{
         			File.Delete(f2);
     			}
-				}
+		}
     	}    
     	
-		/// <summary>
-		/// StartDialog > CreateNewProject (gui)
-		/// </summary>
+	/// <summary>
+	/// StartDialog > CreateNewProject (gui)
+	/// </summary>
     	[UserCodeMethod]
     	public static void StartDialogCreateNewProject()
     	{
-    		var repo = SETUPRepository.Instance;    		
+    		var repo = SETUPRepository.Instance; 
+		
     		repo.StartDialog.CreateNewProjectBtn.Click();
-            Delay.Seconds(15);
+            	Delay.Seconds(15);
     	}
     	
-		/// <summary>
-		///  Open a specific project.
-		/// <param name="patchProject">path to the project.</param>
-		// for example, value="C:/Ranorex/.../Projects/">
-		/// <param name="nameProject">the project name.</param>
-		// for example, value="myProject.docx">
-		/// </summary>
+	/// <summary>
+	///  Open a specific project.
+	/// <param name="patchProject">path to the project.</param>
+	/// for example, value="C:/Ranorex/.../Projects/">
+	/// <param name="nameProject">the project name.</param>
+	/// for example, value="myProject.docx">
+	/// </summary>
     	[UserCodeMethod]
     	public static void OpenProjectStartDialog(string patchProject, string nameProject)
     	{
-    		var repo = SETUPRepository.Instance;    		
-    		repo.StartDialog.OpenProject.Click();
-            Delay.Milliseconds(10);
+		var repo = SETUPRepository.Instance;  
+		
+    	    	repo.StartDialog.OpenProject.Click();
+            	Delay.Milliseconds(10);
             
-            // form ['Open Project']
-            repo.OpenProjectWindow.PreviousLocations.Click();
-            repo.OpenProjectWindow.Text41477.TextValue = patchProject;
-            Delay.Milliseconds(10);  
-            Keyboard.Press("{Return}");
-            repo.OpenProjectWindow.Text1148.Click();
-            repo.OpenProjectWindow.Text1148.TextValue = nameProject;
-			Delay.Milliseconds(10);
-			// All files (*.*)
-			repo.OpenProjectWindow.ComboBox1348.Click();
-			repo.List1000.AllFiles.MoveTo();
-			repo.List1000.AllFiles.Click();
-			repo.OpenProjectWindow.OpenDtn.Click();
-			Delay.Seconds(3);
+            	// form ['Open Project']
+           	repo.OpenProjectWindow.PreviousLocations.Click();
+            	repo.OpenProjectWindow.Text41477.TextValue = patchProject;
+            	Delay.Milliseconds(10);  
+            	Keyboard.Press("{Return}");
+            	repo.OpenProjectWindow.Text1148.Click();
+            	repo.OpenProjectWindow.Text1148.TextValue = nameProject;
+	    	Delay.Milliseconds(10);
+	    	// All files (*.*)
+	    	repo.OpenProjectWindow.ComboBox1348.Click();
+	    	repo.List1000.AllFiles.MoveTo();
+	    	repo.List1000.AllFiles.Click();
+	    	repo.OpenProjectWindow.OpenDtn.Click();
+	    	Delay.Seconds(3);
     	}
     	
-		/// <summary>
-		/// Resize a window to actually monitor resolution.
-		/// </summary>
+	/// <summary>
+	/// Resize a window to actually monitor resolution.
+	/// </summary>
     	[UserCodeMethod]
     	public static void ResizeWindow()
     	{
-			var repo = SETUPRepository.Instance;
+		var repo = SETUPRepository.Instance;
 			
     		Size resolution = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Size;
     		repo.ProjectWindow.Self.Resize(resolution.Width, resolution.Height);
     	}
     	
-		/// <summary> 
-		/// Remove from \FolderResult all files created during the test run.
-		/// <param name="product">the test product name.</param>
-		/// </summary>
+	/// <summary> 
+	/// Remove from \FolderResult all files created during the test run.
+	/// <param name="product">the test product name.</param>
+	/// </summary>
     	[UserCodeMethod]
     	public static void DeleteFilesFromDIR(string product)
     	{
     		string FolderResult = @"c:\\Ranorex\\" + product + "\\FolderResult\\";
     		if (Directory.Exists(Environment.ExpandEnvironmentVariables(FolderResult)))
     		{
-    		DirectoryInfo dirInfo = new DirectoryInfo(Environment.ExpandEnvironmentVariables(FolderResult));
-        	foreach (FileInfo file in dirInfo.GetFiles())
-				{
+    			DirectoryInfo dirInfo = new DirectoryInfo(Environment.ExpandEnvironmentVariables(FolderResult));
+        		foreach (FileInfo file in dirInfo.GetFiles())
+			{
     				file.Delete();
-				}
+			}
     		}
     	}
 		
-		/// <summary>
-		// Send a dump with a comment.
-		/// </summary>
+	/// <summary>
+	/// Send a dump with a comment.
+	/// </summary>
     	[UserCodeMethod]
     	public static void CheckDamp()
     	{
     		var repo = SETUPRepository.Instance;
 			
-			// the dump-client name
+		// the dump-client name
      		string appname = "DmpClientName"; 
 			
-			// process list
-            Process[] procList = Process.GetProcessesByName(appname); 
+		// process list
+           	Process[] procList = Process.GetProcessesByName(appname); 
 			
-			// watching each process
-            foreach (System.Diagnostics.Process anti in procList) 
-			{
+		// watching each process
+            	foreach (System.Diagnostics.Process anti in procList) 
+		{
 			// if the required process has started
-			if(anti.ProcessName.Contains(appname)) 
-			
+			if(anti.ProcessName.Contains(appname))
 			{
-     			Report.Info("Dump-client has existed");
-     			repo.DmpClient.Comment.Click();
-     			repo.DmpClient.Comment.TextValue = "has found by autotest";
-     			Delay.Milliseconds(10);
+     				Report.Info("Dump-client has existed");
+     				repo.DmpClient.Comment.Click();
+     				repo.DmpClient.Comment.TextValue = "has found by autotest";
+     				Delay.Milliseconds(10);
 				repo.DmpClient.Send.Click(); 
 				Delay.Milliseconds(10);
-     		}
-     		else
-     		{
-     			Report.Info("Dump-client hasn't existed");
-     		}
+     			}
+     			else
+     			{
+     				Report.Info("Dump-client hasn't existed");
+     			}
     		}
     	}
     	
-		/// <summary>
-		/// Create a ResultFolder for each test product.
-		/// <param name="product">the test product name.</param>
-		/// </summary>
+	/// <summary>
+	/// Create a ResultFolder for each test product.
+	/// <param name="product">the test product name.</param>
+	/// </summary>
     	[UserCodeMethod]
     	public static void CreateFolderResult(string product)
     	{
-    		string FolderResult = "c:\\Ranorex\\" + product + "\\FolderResult\\";
-            FileSystem.CreateDirectory(FolderResult);
-            Delay.Milliseconds(10);
+		string FolderResult = "c:\\Ranorex\\" + product + "\\FolderResult\\";
+           	FileSystem.CreateDirectory(FolderResult);
+            	Delay.Milliseconds(10);
     	}  
     }
 }
